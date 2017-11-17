14d8133a1d881a9836dfb30934dab012c4011e10
Granton Mchana <gmchana254@gmail.com>
2017-11-17 16:15:12 +0300

    fixing bug

--- photostudio/picture/models.py
+++ photostudio/picture/models.py
@@ -3,10 +3,45 @@ from django.db import models
 # Create your models here.
 
 
-class Image(models.Model):
+class Post(models.Model):
     title = models.CharField(max_length=30)
-    image_link = models.CharField(max_length=250)
-    image_image = models.ImageField(upload_to='images/', null=True)
+    image_link = models.CharField(max_length=60)
+    post_image = models.ImageField(upload_to='posts/', null=True)
+    tags = models.ManyToManyField(tags)
+    user = models.ForeignKey(User)
 
     def __str__(self):
         return self.title
+
+    def save_post(self):
+        self.save()
+
+    @classmethod
+    def get_posts(cls):
+        posts = cls.posts.all()
+
+        return posts
+
+
+class tags(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.first_name
+
+    def save_user(self):
+        self.save()
+
+    @classmethod
+    def get_users(cls):
+        users = cls.objects.all()
+
+        return users
--- photostudio/picture/views.py
+++ photostudio/picture/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Image
+from .models import Post
 
 # Create your views here.
 
 
 def index(request):
-    images = Image.get_images()
+    posts = Post.get_posts()
 
-    return render(request, 'all-images/index.html', {"images": images})
+    return render(request, 'all-posts/index.html', {"posts": posts})
+
+
+def post(request, post_id):
+
+    try:
+        post = Post.objects.get(id=post_id)
+        tags = post.tags.all()
+
+    except DoesNotExist:
+        raise Http404()
+
+    return render(request, 'all-posts/post.html', {"post": post, "tags": tags})
+
+
+def search_results(request):
+    if 'post' in request.GET and request.GET["post"]:
+        search_term = request.GET.get("post")
+        searched_posts = Post.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-posts/search.html', {"message": message, "posts": searched_posts})
