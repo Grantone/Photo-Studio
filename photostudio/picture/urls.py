f220d9b997208721d5af47a9d16cf2e75fb5fceb
Granton Mchana <gmchana254@gmail.com>
2017-11-18 12:36:28 +0300

    additions

--- photostudio/picture/urls.py
+++ photostudio/picture/urls.py
@@ -1,4 +1,4 @@
-# from django.conf import settings
+from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url
 from . import views
@@ -11,6 +11,6 @@ urlpatterns = [
     url(r'^post/(\d+)', views.post, name='post'),
 
 ]
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL,
-#                           document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
